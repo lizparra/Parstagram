770e2e2a8d041c9086f0a71c9f345bfc79f13130
lizparra <44618559+lizparra@users.noreply.github.com>
2020-07-17 23:39:08 -0400

    Added: User stays logged in across restarts, User can log out, User can view comments on a post, User can add a new comment.

---
+++ Parstagram/CommentCell.swift
@@ -0,0 +1,27 @@
+//
+//  CommentCell.swift
+//  Parstagram
+//
+//  Created by Liz Parra on 7/17/20.
+//  Copyright © 2020 lizparra. All rights reserved.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var commentLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
